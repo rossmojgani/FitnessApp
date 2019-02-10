13193a5201a9842ed3bf20635da7d2ad8d9e2bfb
adammet <adam13met@gmail.com>
2019-02-09 22:17:47 -0800

    Login Page Username and Password field

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Main from './components/main';
-import { Link } from 'react-router-dom';
+
 
 class App extends Component {
   render() {
