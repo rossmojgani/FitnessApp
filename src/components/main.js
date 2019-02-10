ab3e572bbf71b5c86abaff4bd8972774cfc89e96
Ross <rmojgani99@gmail.com>
2019-02-09 22:19:48 -0800

    initial default page

--- src/components/main.js
+++ src/components/main.js
@@ -2,11 +2,13 @@ import React from 'react';
 
 import { HashRouter, Switch, Route } from 'react-router-dom';
 import LoginPage from './loginpage';
+import DefaultPage from './defaultpage';
 
 const Main = () => (
     <HashRouter>
         <Switch>
             <Route exact path="/" component={LoginPage} />
+            <Route exact path="/main" component={DefaultPage} />
         </Switch>
     </HashRouter>
 )
