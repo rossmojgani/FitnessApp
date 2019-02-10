92284a14d3fe3399da93fbb44d412da8a637d3e5
Ross <rmojgani99@gmail.com>
2019-02-09 19:10:37 -0800

    added mainpage.js

---
+++ src/components/mainpage.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+class mainpage extends components () {
+
+    render() {
+
+        return (
+            <header>
+                MAINPAGE
+            </header>
+
+
+
+
+        ) 
+
+
+
+    
+    }
+
+
+
+
+
+
+}
