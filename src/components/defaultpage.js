f00a4266c84d1b7fdae55083ec615f9385d1dece
Ross <rmojgani99@gmail.com>
2019-02-10 02:32:55 -0800

    basic mainpage working

--- src/components/defaultpage.js
+++ src/components/defaultpage.js
@@ -1,22 +1,44 @@
 import React, { Component } from 'react';
 import {Link} from "react-router-dom";
+import {Button} from 'reactstrap';
+import logo from './Logo.png';
+//import ButtonToolbar from 'react-bootstrap/ButtonToolbar'
 
 class DefaultPage extends Component {
 
     render() {
 
         return (
-            <div>
-          <h1>Fitness Friend</h1>
+          <div>
+          <div class="row">
+            <div class="column">
+                    <img src={logo} width="200" height="100" />
+            <div class="column">
+                <h1>Fitness Friend</h1>
+            </div>
+          </div>
+
+
+          </div>
           <ul className="header">
             <li><Link to="/main/fitness">Fitness</Link></li>
-            <li><Link to="/main/Nutrition">Nutrition</Link></li>
-            <li><Link to="/main/Info">Info</Link></li>
+            <li><Link to="/main/nutrition">Nutrition</Link></li>
+            <li><Link to="/main/info">Info</Link></li>
           </ul>
           <div className="content">
-             
+          <div>
+          <Button>
+          Fitness Plan
+          </Button>
+          <Button>
+          Nutrition Plan
+          </Button>
+          <Button>
+              Edit Info
+          </Button>
           </div>
-        </div>
+          </div>
+          </div>
         ) 
     }
 
