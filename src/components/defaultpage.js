02f7a72ba82e89b4d91b7fb435d9182b1f8fc623
Ross <rmojgani99@gmail.com>
2019-02-09 23:04:00 -0800

    basic functionality header

--- src/components/defaultpage.js
+++ src/components/defaultpage.js
@@ -1,46 +1,22 @@
 import React, { Component } from 'react';
-import { Navbar} from 'react-bootstrap';
+import {Link} from "react-router-dom";
 
 class DefaultPage extends Component {
 
     render() {
 
         return (
-            
-                <>
-                <Navbar bg="light">
-                    <Navbar.Brand href="#home">Brand link</Navbar.Brand>
-                </Navbar>
-                <br />
-                <Navbar bg="light">
-                    <Navbar.Brand>Brand text</Navbar.Brand>
-                </Navbar>
-                <br />
-                <Navbar bg="dark">
-                    <Navbar.Brand href="#home">
-                    <img
-                        src="/logo.svg"
-                        width="30"
-                        height="30"
-                        className="d-inline-block align-top"
-                        alt="React Bootstrap logo"
-                    />
-                    </Navbar.Brand>
-                </Navbar>
-                <br />
-                <Navbar bg="dark" variant="dark">
-                    <Navbar.Brand href="#home">
-                    <img
-                        alt=""
-                        src="/logo.svg"
-                        width="30"
-                        height="30"
-                        className="d-inline-block align-top"
-                    />
-                    {' React Bootstrap'}
-                    </Navbar.Brand>
-                </Navbar>
-                </>
+            <div>
+          <h1>Fitness Friend</h1>
+          <ul className="header">
+            <li><Link to="/main/fitness">Fitness</Link></li>
+            <li><Link to="/main/Nutrition">Nutrition</Link></li>
+            <li><Link to="/main/Info">Info</Link></li>
+          </ul>
+          <div className="content">
+             
+          </div>
+        </div>
         ) 
     }
 
