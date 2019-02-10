c4a902fb297e158007b20930f9727102d2754b43
adammet <adam13met@gmail.com>
2019-02-10 03:19:15 -0800

    A lot of stuff changed to login page

--- src/components/loginpage.js
+++ src/components/loginpage.js
@@ -1,37 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
-
 class LoginPage extends Component {
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            isLoginOpen: true,
-            isRegisterOpen: false
-        };
-    }
-
-    render() {
-
-        return (
-            <div className = "root-container">
-            <center>
-            <div className="box-container">
-            {this.state.isLoginOpen && <LoginBox/>
-            }
-        {this.state.isRegisterOpen && <RegisterBox/>}
-    </div>
-            </center>
-            </div>
-        );
-    }
-}
-
-//reactDOM.render(
-    //<LoginPage />, document.getElementById("root"));
-
-class LoginBox extends Component {
 
     constructor(props) {
         super(props);
@@ -42,6 +12,9 @@ class LoginBox extends Component {
 
     render() {
         return (
+            <div className = "root-container">
+                <center>
+                    <div className = "box-container">
             <div className = "inner-container">
                 <div className = "login_header">
                     Welcome to Fitness Friend!
@@ -72,73 +45,21 @@ class LoginBox extends Component {
                         type = "button"
                         className = "login-btn"
                         onClick = {this.submitLogin.bind(this)}><Link to="/main" >Login</Link></button>
-                </div>
-            </div>
-        );
-    }
-}
 
-class RegisterBox extends React.Component {
-
-    constructor(props) {
-        super(props);
-        this.state = {};
-    }
-
-    submitRegister(e) {}
-
-    render() {
-        return (
-            <div className="inner-container">
-                <div className="header">
-                    Register
-                </div>
-                <div className="box">
-
-                    <div className="input-group">
-                        <label htmlFor="username">Username</label>
-                        <input
-                            type="text"
-                            name="username"
-                            className="login-input"
-                            placeholder="Username"/>
-                    </div>
-
-                    <div className="input-group">
-                        <label htmlFor="email">Email</label>
-                        <input type="text" name="email" className="login-input" placeholder="Email"/>
-                    </div>
-
-                    <div className="input-group">
-                        <label htmlFor="password">Password</label>
-                        <input
-                            type="password"
-                            name="password"
-                            className="login-input"
-                            placeholder="Password"/>
-                    </div>
                     <button
-                        type="button"
-                        className="login-btn"
-                        onClick={this
-                            .submitRegister
-                            .bind(this)}>Register</button>
+                        type = "button"
+                        className = "login-btn"
+                        >
+                         <Link to="/registerpage" > Create New User </Link>
+                    </button>
                 </div>
+            </div>
+            </div>
+            </center>
             </div>
         );
     }
 }
-//
-// class LoginPage extends Component{
-//     render() {
-//     return (
-//         <div className="box-container">
-//             {this.state.isLoginOpen && <LoginBox/>
-//             }
-//             {this.state.isRegisterOpen && <RegisterBox/>}
-//         </div>
-//     );
-//   }
-// }
+
 
 export default LoginPage;
